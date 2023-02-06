2f0ad7400ec772c41872fa27463f4d0b98eed2d6
Hamilton <>
2023-02-06 20:38:22 +0100

    refactor and lazyload images

--- src/features/misc/Header.js
+++ src/features/misc/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = React.memo(() => {
-  return <h1 className="header">RobotFriends</h1>;
+  return <h1>RobotFriends</h1>;
 })
 
 export default Header
--- src/features/robots/Card.js
+++ src/features/robots/Card.js
@@ -4,8 +4,12 @@ const Card = ({ id, name, email, selected, hidden }) => {
   const isHidden = () => (hidden === null || hidden === id ? "" : "hidden");
   const onClick = () => {
     return selected(id);
-  };
-
+    };
+  const lazyOrAuto = (id) => {
+    if(id>6){return "lazy"}
+    return "auto"
+  }
+  
   return (
     <div
       className={`bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 card ${isHidden()}`}
@@ -15,9 +19,9 @@ const Card = ({ id, name, email, selected, hidden }) => {
         alt={`robot-${id}`}
         src={`https://robohash.org/${id}?size=200x200`}
         id={id}
-        loading="lazy"
-        height="200px"
-        width="200px"
+        loading={lazyOrAuto(id)}
+        height={"200px"}
+        width={"200px"}
       />
       <div>
         <h2>{name}</h2>
